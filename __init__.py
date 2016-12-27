fc3a04c243685d84d5a5ded5ce1f727ebe143816
Nathanael Farley <nasfarley88@gmail.com>
2016-12-27 14:17:50 +0000

    Works as basic shopping list now.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
-from telepot import glance
+from telepot import glance, origin_identifier
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-from skybeard.beards import BeardChatHandler, Filters
+from skybeard.beards import BeardChatHandler, Filters, ThatsNotMineException
 import logging
+import re
 
 
 logger = logging.getLogger(__name__)
@@ -17,6 +18,7 @@ class ShoppingListBeard(BeardChatHandler):
         (Filters.text, 'pprint_list', 'Echos everything said by anyone.')
     ]
 
+    shopping_list_prefix = "Shopping list:"
     item_prefix = " ☐ "
     item_done_prefix = " ☑ "
 
@@ -47,7 +49,7 @@ class ShoppingListBeard(BeardChatHandler):
 
         text = [x.strip() for x in text.split(",")]
         keyboard = self.make_keyboard(text)  # At this point text is a list
-        text = ['Shopping list:'] + text
+        text = [ShoppingListBeard.shopping_list_prefix] + text
         text = "\n{}".format(ShoppingListBeard.item_prefix).join(text)
         await self.sender.sendMessage(text, reply_markup=keyboard)
 
@@ -62,5 +64,49 @@ class ShoppingListBeard(BeardChatHandler):
         await self.edit_shopping_list(msg, data)
 
     async def edit_shopping_list(self, origin_msg, data):
-        await self.sender.sendMessage("Origin message:\n"+str(origin_msg))
-        await self.sender.sendMessage("Data: "+str(data))
+        if logger.getEffectiveLevel() == logging.DEBUG:
+            await self.sender.sendMessage("Origin message:\n"+str(origin_msg))
+            await self.sender.sendMessage("Shopping list as list:\n"+str(
+                self.parse_shopping_list(origin_msg['message']['text'])))
+            await self.sender.sendMessage("Data: "+str(data))
+
+        try:
+            data = int(data)
+        except Exception as e:
+            self.sender.sendMessage("Sorry, something went wrong.")
+            raise e
+
+        # Process the text
+        shopping_list = origin_msg['message']['text'].split("\n")[1:]
+        if ShoppingListBeard.item_prefix in shopping_list[data]:
+            shopping_list[data] = shopping_list[data].replace(
+                ShoppingListBeard.item_prefix,
+                ShoppingListBeard.item_done_prefix,
+            )
+        elif ShoppingListBeard.item_done_prefix in shopping_list[data]:
+            shopping_list[data] = shopping_list[data].replace(
+                ShoppingListBeard.item_done_prefix,
+                ShoppingListBeard.item_prefix,
+            )
+        else:
+            assert False, "Hmm, shouldn't get here..."
+
+        text = [ShoppingListBeard.shopping_list_prefix] + shopping_list
+        text = "\n".join(text)
+
+        shopping_list_items = self.parse_shopping_list(
+            origin_msg['message']['text'])
+        keyboard = self.make_keyboard(shopping_list_items)
+
+        await self.bot.editMessageText(
+            origin_identifier(origin_msg),
+            text=text,
+            reply_markup=keyboard,
+        )
+
+    def parse_shopping_list(self, text):
+        pattern = "\n{}|\n{}".format(ShoppingListBeard.item_prefix,
+                                     ShoppingListBeard.item_done_prefix)
+        shopping_list = re.split(pattern, text)
+
+        return shopping_list[1:]
